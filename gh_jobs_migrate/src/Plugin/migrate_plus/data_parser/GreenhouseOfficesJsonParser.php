71cc97a7fda99ce76934ea4db08923e53834dbc3
Adriano Pulz <adriano.pulz@arctouch.com>
2019-09-08 16:03:21 -0300

    Add Offices migration.

---
+++ gh_jobs_migrate/src/Plugin/migrate_plus/data_parser/GreenhouseOfficesJsonParser.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\gh_jobs_migrate\Plugin\migrate_plus\data_parser;
+
+use Drupal\migrate_plus\Plugin\migrate_plus\data_parser\Json;
+
+/**
+ * Obtain JSON data for departments taxonomy.
+ *
+ * @DataParser(
+ *   id = "greenhouse_offices_json_parser",
+ *   title = @Translation("Greenhouse Offices Json Parser")
+ * )
+ */
+class GreenhouseOfficesJsonParser extends Json {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    // Build and set the request URL.
+    $this->urls = [
+      $this->prepareGhBoardUrl($this->urls[0]),
+    ];
+  }
+
+  /**
+   * Prepares a given URL for an Jobs Boards web service call.
+   *
+   * @param string $url
+   *   The Jobs Board endpoint URL with board placeholder value.
+   *
+   * @return string
+   *   The URL with placeholder values replaced.
+   */
+  private function prepareGhBoardUrl($url) {
+    $config = \Drupal::config(GH_JOBS_SETTINGS);
+    $board = $config->get(BOARD_TOKEN_CONFIG_NAME);
+
+    return str_replace("{board_token}", $board, $url);
+  }
+
+}
